e7788938064cac5d41efe30adf095f3f8c7d0fe0
Jonathan Geoffroy <geoffroy.jonathan@gmail.com>
2025-04-10 22:10:10 +0200

    chore: rotate player to follow the cursor

--- player/player.gd
+++ player/player.gd
@@ -7,9 +7,20 @@ extends CharacterBody2D
 
 
 func _physics_process(delta: float) -> void:
+	move(delta)
+	look_at_mouse(delta)
+
+
+func move(delta):
 	var direction = Input.get_vector("left", "right", "up", "down")
 	if direction != Vector2.ZERO:
 		velocity = velocity.move_toward(direction * max_speed, acceleration * delta)
 	else:
 		velocity = velocity.move_toward(direction * max_speed, deceleration * delta)
+
 	move_and_slide()
+
+
+func look_at_mouse(delta):
+	var mouse_position = get_global_mouse_position()
+	look_at(mouse_position)
